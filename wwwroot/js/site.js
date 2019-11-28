012e0f92776ff186a4a612935fb0d137015947cf
Andrew Ettensohn <andrewettensohn@gmail.com>
2019-11-27 19:02:35 -0500

    Finished feature.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -27,7 +27,7 @@ function addItem() {
         body: JSON.stringify(item)
     })
         .then(response => response.json())
-        .then(data => displayNewItem(data))
+        .then(data => displayNewItem())
         .then(() => {
             addNameTextbox.value = '';
         })
@@ -35,67 +35,12 @@ function addItem() {
 
 } 
 
-function displayNewItem(data) {
+function displayNewItem() {
 
-    var item = data;
 
-    var taskHTML = `
-            <!--Accordion | Parent Task Accordion -->
-            <div id="taskAccordion${item.id}">
-                <div class="card bg-dark">
-                    <div class="card-header" id="taskHeading${item.id}">
-                        <img id="statusIcon${item.id}" class="float-left" height="80" width="8" src="lib/statusIcons/${item.taskStatus}.png" />
-                        <h1 class="mb-0 float-left">
-                            <button class="btn text-white" data-toggle="collapse" data-target="#taskCollapse${item.id}" aria-expanded="false" aria-controls="taskCollapse${item.id}">
-                                <h5 id="taskNameHeader${item.id}">${item.taskName}</h5>
-                            </button>
-                        </h1>
-                        <div class="dropdown show">
-                            <a class="btn text-white dropdown-toggle float-left" href="#" role="button" id="statusDropDown${item.id}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                ${item.taskStatus}
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="statusDropDown${item.id}">
-                                <a class="dropdown-item" onclick="taskStatusChange('Not Started', '${item.id}', '${item.taskName}')" >Not Started</a>
-                                <a class="dropdown-item" onclick="taskStatusChange('In-Progress', '${item.id}', '${item.taskName}')" >In-Progress</a>
-                                <a class="dropdown-item" onclick="taskStatusChange('Completed', '${item.id}', '${item.taskName}')">Completed</a>
-                            </div>
-                        </div>
-                    </div>
-                        <div id="subAccordion${item.id}">
-                            <div class="card bg-dark">
-                                <div class="card-header" id="subHeading${item.id}">
-                                    <img class="float-left" height="40" width="8" src="lib/statusIcons/iconNotStarted.png" />
-                                    <h5 class="mb-0 float-left">
-                                        <button class="btn text-white" data-toggle="collapse" id="subTaskNameHeader${item.id}" data-target="#subCollapse${item.id}" aria-expanded="false" aria-controls="subCollapse${item.id}">
-                                            ${item.taskName}
-                                        </button>
-                                    </h5>
-                                    <div class="dropdown show">
-                                        <a class="btn text-white dropdown-toggle float-left" href="#" role="button" id="subStatusDropDown1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            In-Progress
-                                        </a>
+    $("#output").empty();
 
-                                        <div class="dropdown-menu" aria-labelledby="subStatusDropDown1">
-                                            <a class="dropdown-item" href="#">Not Started</a>
-                                            <a class="dropdown-item" href="#">In-Progress</a>
-                                            <a class="dropdown-item" href="#">Completed</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div id="subCollapse${item.id}" class="collapse bg-dark" aria-labelledby="subHeading1" data-parent="#subAccordion${item.id}">
-                                    <br />
-                                    <p>Using Bootstrap accordions as an alternative to tables.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            `;
-
-    var newDiv = document.createElement('div');
-    newDiv.innerHTML = taskHTML;
-    document.getElementById("output").appendChild(newDiv);
+    getItems();
 
     $("#add-name").focus();
 
@@ -125,6 +70,20 @@ function taskStatusChange(newStatus, itemId, itemTaskName) {
 
 }
 
+function displayTaskInput(itemId) {
+
+    $(`#btnTaskDropDown${itemId}`).addClass('d-none');
+    $(`#areaInputNameChange${itemId}`).toggleClass('d-none');
+    $(`#inputNameChange${itemId}`).focus();
+}
+
+function taskNameChange(itemId) {
+
+    var newTaskName = $(`inputNameChange${itemId}`).value
+
+}
+
+
 
 function displayItems(data) {
 
@@ -136,11 +95,17 @@ function displayItems(data) {
                 <div class="card bg-dark">
                     <div class="card-header" id="taskHeading${item.id}">
                         <img id="statusIcon${item.id}" class="float-left" height="80" width="8" src="lib/statusIcons/${item.taskStatus}.png" />
-                        <h1 class="mb-0 float-left">
-                            <button class="btn text-white" data-toggle="collapse" data-target="#taskCollapse${item.id}" aria-expanded="false" aria-controls="taskCollapse${item.id}">
+                        <div class="mb-0 float-left" id="divTaskName${item.id}">
+                            <button class="btn text-white" id="btnTaskDropDown${item.id}" data-toggle="collapse" data-target="#taskCollapse${item.id}" aria-expanded="false" aria-controls="taskCollapse${item.id}">
                                 <h5 id="taskNameHeader${item.id}">${item.taskName}</h5>
                             </button>
-                        </h1>
+                            <div class="input-group mb-3 d-none" id="areaInputNameChange${item.id}">
+                              <input id="inputNameChange${item.id}" onfocusout="taskNameChange('${item.id}')" type="text" class="form-control bg-dark text-white border-0" aria-describedby="basic-addon2">
+                              <div class="input-group-append">
+                              </div>
+                            </div>
+                            <br>
+                        </div>
                         <div class="dropdown show">
                             <a class="btn text-white dropdown-toggle float-left" href="#" role="button" id="statusDropDown${item.id}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 ${item.taskStatus}
@@ -151,6 +116,16 @@ function displayItems(data) {
                                 <a class="dropdown-item" onclick="taskStatusChange('Completed', '${item.id}', '${item.taskName}')" href="#">Completed</a>
                             </div>
                         </div>
+                        <button class="btn text-white float-right" id="btnDeleteTask${item.id}">
+                            <svg id="i-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="15" height="15" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                <path d="M2 30 L30 2 M30 30 L2 2" />
+                            </svg>
+                        </button>
+                        <button class="btn text-white float-right" id="btnEditTaskName${item.id}" onclick="displayTaskInput('${item.id}')">
+                            <svg id="i-edit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="15" height="15" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                <path d="M30 7 L25 2 5 22 3 29 10 27 Z M21 6 L26 11 Z M5 22 L10 27 Z" />
+                            </svg>
+                        </button>
                     </div>
                     <div id="taskCollapse${item.id}" class="collapse bg-secondary" aria-labelledby="taskHeading${item.id}" item-parent="#taskAccordion${item.id}">
                         <div id="subAccordion${item.id}">
